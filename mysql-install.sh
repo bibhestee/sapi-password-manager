9cd6826ddfdf9e543f8c8f9b1f2545c07a4be124
Bibest <bibhestee@gmail.com>
2023-07-04 23:46:59 +0100

    mysql script uncommented

--- mysql-install.sh
+++ mysql-install.sh
@@ -1,8 +1,8 @@
-#!/bin/bash
+#!/usr/bin/bash
 
-# sudo apt install mysql-server -y
-# sudo usermod -d /var/lib/mysql/ mysql
-# sudo service mysql start
-# sudo service mysql status
+sudo apt install mysql-server -y
+sudo usermod -d /var/lib/mysql/ mysql
+sudo service mysql start
+sudo service mysql status
 cat db_setup.sql | sudo mysql -uroot 
 
