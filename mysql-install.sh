bdd68614142558309e6ffaa178eccfdcd8ca4333
Abdulbasit Abdullahi <Bibhestee@gmail.com>
2023-07-04 19:59:09 +0000

    mysql installation script updated

--- mysql-install.sh
+++ mysql-install.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-sudo apt install mysql-server -y
-sudo service mysql start
-sudo service mysql status
+# sudo apt install mysql-server -y
+# sudo usermod -d /var/lib/mysql/ mysql
+# sudo service mysql start
+# sudo service mysql status
 cat db_setup.sql | sudo mysql -uroot 
 
