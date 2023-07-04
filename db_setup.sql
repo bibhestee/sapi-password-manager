b1289649ba6890eadd098a24ee1e4bb3ee2fb9da
Abdulbasit Abdullahi <Bibhestee@gmail.com>
2023-07-04 14:42:09 +0000

    modify the validate password variable typo

--- db_setup.sql
+++ db_setup.sql
@@ -4,7 +4,7 @@
 
 select plugin_name, plugin_status from information_schema.plugins where plugin_name like 'validate%';
 install plugin validate_password soname 'validate_password.so';
-SET GLOBAL validate_password.policy=LOW;
+SET GLOBAL validate_password_policy=LOW;
 CREATE DATABASE IF NOT EXISTS sapi_db;
 CREATE USER IF NOT EXISTS 'sapi'@'localhost' IDENTIFIED BY 'sapi_v1.0';
 GRANT ALL PRIVILEGES ON sapi_db.* TO 'sapi'@'localhost';
