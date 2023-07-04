9b92600a320dd1ea4a26936c601a9a86c0dee19e
Abdulbasit Abdullahi <Bibhestee@gmail.com>
2023-07-04 08:53:05 +0000

    MySQL: install plugin validate password added

--- db_setup.sql
+++ db_setup.sql
@@ -2,6 +2,8 @@
 -- create a user and password
 -- grants necessary permissions on sapi
 
+select plugin_name, plugin_status from information_schema.plugins where plugin_name like 'validate%';
+install plugin validate_password soname 'validate_password.so';
 SET GLOBAL validate_password.policy=LOW;
 CREATE DATABASE IF NOT EXISTS sapi_db;
 CREATE USER IF NOT EXISTS 'sapi'@'localhost' IDENTIFIED BY 'sapi_v1.0';
