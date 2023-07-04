335a436d2dd0a43bba7aaac9fed972cb1b2532f2
Bibest <bibhestee@gmail.com>
2023-07-04 23:44:08 +0100

    security question added to the validate schema

--- API/validators/signup.js
+++ API/validators/signup.js
@@ -2,16 +2,18 @@
 
 const Joi = require('joi');
 
-async function validateSignUp(username, email, password) {
+async function validateSignUp(username, email, password, securityQuestion) {
   const schema = Joi.object({
     username: Joi.string().min(3).max(12).required(),
     password: Joi.string().min(8).required(),
-    email: Joi.string().email().regex(/^[a-zA-Z0-9_.+-]+@(gmail|yahoo)\.com$/).required()
+    email: Joi.string().email().regex(/^[a-zA-Z0-9_.+-]+@(gmail|yahoo)\.com$/).required(),
+    securityQuestion: Joi.string().min(3).max(15).required(),
   });
   const data = {
     username,
     email,
     password,
+    securityQuestion,
   }
   const result = await schema.validate(data);
 
